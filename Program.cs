c6b786b9913499233a4dddc8591100ae3d58fe64
Abram Molinar <a361968@uach.mx>
2025-06-20 19:34:59 -0600

    app hola c#: 1.0

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola desde la aplicación C#!");
+            Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
+
